3c02d49772980c19b43aeedf7382282297f57553
Kiraabs <133869408+Kiraabs@users.noreply.github.com>
2023-05-19 16:45:16 +0300

    Add files via upload

---
+++ ПР11 - Анимация изображений/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Анимация
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.buttonBegin = new System.Windows.Forms.Button();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Location = new System.Drawing.Point(12, 52);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(460, 303);
+            this.pictureBoxImage.TabIndex = 0;
+            this.pictureBoxImage.TabStop = false;
+            // 
+            // buttonBegin
+            // 
+            this.buttonBegin.Location = new System.Drawing.Point(12, 1);
+            this.buttonBegin.Name = "buttonBegin";
+            this.buttonBegin.Size = new System.Drawing.Size(79, 45);
+            this.buttonBegin.TabIndex = 1;
+            this.buttonBegin.Text = "Старт";
+            this.buttonBegin.UseVisualStyleBackColor = true;
+            this.buttonBegin.Click += new System.EventHandler(this.buttonBegin_Click);
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Location = new System.Drawing.Point(97, 1);
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(375, 45);
+            this.trackBarSpeed.TabIndex = 2;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.buttonBegin);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Name = "Form1";
+            this.Text = "Анимация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.Button buttonBegin;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ ПР11 - Анимация изображений/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Анимация
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Bitmap[] Frames;
+        private int FrameNum = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Frames = new Bitmap[18];
+            for (int i = 0; i < 18; i++) Frames[i] = new Bitmap($"Frame{i}.png");
+            pictureBoxImage.Image = Frames[FrameNum];
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            FrameNum = ++FrameNum % Frames.Length;
+            pictureBoxImage.Image = Frames[FrameNum];
+        }
+        private void buttonBegin_Click(object sender, EventArgs e)
+        {
+            timer.Enabled = !timer.Enabled;
+            if (timer.Enabled) buttonBegin.Text = "Стоп";
+            else buttonBegin.Text = "Старт";
+        }
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+            timer.Interval = 1000 / trackBarSpeed.Value;
+        }
+    }
+}
